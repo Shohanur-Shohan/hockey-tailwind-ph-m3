e90bea0593b3cf3553c87d9d26f10765f4708b86
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-01-24 14:20:10 +0600

    program section done with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-        
+        // backgroundImage: {
+        //   'program-card1': "url('/Images/2.png')",
+        //   'program-card2': "url('/Images/3.png')",
+        //   'program-card3': "url('/Images/4.png')",
+        // }
       },
      
     },
